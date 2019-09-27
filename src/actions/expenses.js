b8d5297a673893b47e3d43f1b332c14856c04ac9
Marc Selles <marcselles@MacBook-Pro-van-Marc.local>
2019-09-27 14:44:42 +0200

    Fetch data from firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,4 +1,3 @@
-import uuid from 'uuid';
 import database from '../firebase/firebase';
 
 // Component calls action generator
@@ -43,3 +42,27 @@ export const editExpense = ( id, updates ) => ({
     id,
     updates
 });
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+    type: 'SET_EXPENSES',
+    expenses
+});
+
+export const startSetExpenses = () => {
+    return (dispatch) => {
+        return database.ref('expenses')
+            .once('value')
+            .then((snapshot) => {
+                const databaseExpenses = [];
+                snapshot.forEach((childSnapshot) => {
+                    databaseExpenses.push({
+                    id: childSnapshot.key,
+                    ...childSnapshot.val()
+                    });
+                });
+
+                dispatch(setExpenses(databaseExpenses));
+            })
+        };
+};
